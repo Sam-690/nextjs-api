f0d606d1b84ff15630a817bd3a6fd33993cec7b9
Sam Martinez <56923470+Sam-690@users.noreply.github.com>
2021-11-02 10:33:18 -0600

    mongo coneccion

--- src/pages/tasks/new.js
+++ src/pages/tasks/new.js
@@ -31,7 +31,7 @@ export default function ProductFormPage() {
     
     const createProduct = async () => {
         try {
-            await fetch('http://localhost:3000/api/tasks', { 
+            await fetch('https://nextjs-apprest.herokuapp.com/api/tasks', { 
                 method: 'POST', 
                 headers: {
                     "Content-Type": "application/json"
